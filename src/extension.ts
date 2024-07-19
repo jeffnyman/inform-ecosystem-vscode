46b16ebdc3f215e16bf5f9b1bbca545414ca5282
Jeff Nyman <jeffnyman@gmail.com>
2024-07-19 04:32:13 -0500

    Add testing command.

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,12 @@ import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log("Inform Ecosystem Extension enabled.");
+
+  let disposable = vscode.commands.registerCommand("infeco.testing", () => {
+    vscode.window.showInformationMessage("Inform Ecosystem Active.");
+  });
+
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
